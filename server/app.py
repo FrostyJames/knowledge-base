aec3d2afb51bd381b3770e7287cd5573f270d7dc
Edwin Mwaniki <edwin.mwaniki@student.moringaschool.com>
2025-09-23 23:17:19 +0300

    Imported cors and restful

--- server/app.py
+++ server/app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import MetaData
+from flask_cors import CORS 
+from flask_migrate import Migrate
+from flask_restful import Api
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -10,8 +14,20 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///knowbase.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    # Define metadata, instantiate db
+    metadata = MetaData(naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    })
+
     db.init_app(app)
     migrate.init_app(app, db)
+
+    # Instantiate REST API
+    api = Api(app)
+
+    # Instantiate CORS
+    CORS(app)
+
 
     # Import models so migrations see them
     from models import User, Role, Permission, Category, Article, Tag, ArticleMedia
