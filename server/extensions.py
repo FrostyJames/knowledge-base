92efdb1aaee89cd54f87313a12620f74506b4648
Edwin Mwaniki <edwin.mwaniki@student.moringaschool.com>
2025-09-24 11:18:28 +0300

    Updated the Metadata Conventions

--- server/extensions.py
+++ server/extensions.py
@@ -3,8 +3,14 @@ from flask_migrate import Migrate
 from sqlalchemy import MetaData
 
 convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
 }
 
-db = SQLAlchemy(metadata=MetaData(naming_convention=convention))
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(metadata=metadata)
+
 migrate = Migrate()
