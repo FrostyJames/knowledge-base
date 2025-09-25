c98ea94495dba3a42b3c8492f9bcb20042254054
Edwin Mwaniki <edwin.mwaniki@student.moringaschool.com>
2025-09-25 20:00:42 +0300

    Added configurations media

--- server/app_routes/media.py
+++ server/app_routes/media.py
@@ -4,6 +4,14 @@ from werkzeug.utils import secure_filename
 from extensions import db
 from models import ArticleMedia
 
+# Configure uploads
+UPLOAD_FOLDER = "uploads/"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif", "mp4", "mov", "pdf", "docx"}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 def register_media_routes(app):
 
     # GET all media
