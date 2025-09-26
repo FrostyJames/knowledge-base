3505f97f97ee8eb0b02ffb44bbc109245ec6457b
edwin3v3 <edwin.mwaniki@student.moringaschool.com>
2025-09-26 11:01:10 +0300

    Update import upgrade app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 from flask_cors import CORS
 from flask_restful import Api
 from extensions import db, migrate
