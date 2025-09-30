a313aee9041946e696645f95bf4d4609304be24b
James Ivan <ivanlavan773@gmail.com>
2025-09-30 05:45:55 +0300

    Switch to python for Railway compatibility

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd server
 export FLASK_APP=app:app
-python3 -m flask db upgrade
-python3 -m gunicorn app:app --bind 0.0.0.0:$PORT
+python -m flask db upgrade
+python -m gunicorn app:app --bind 0.0.0.0:$PORT
